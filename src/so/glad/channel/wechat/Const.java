2b57ba4d42c810c95ee926d9b69ad7d7fcbff162
palmtale <m@cheku.co>
2015-04-12 03:39:22 +0800

    Initial

---
+++ src/so/glad/channel/wechat/Const.java
@@ -0,0 +1,8 @@
+package so.glad.channel.wechat;
+
+/**
+ * @author Cartoon
+ *         on 2015/4/9.
+ */
+public class Const {
+}
---
+++ src/so/glad/channel/wechat/api/WeChat.java
@@ -0,0 +1,8 @@
+package so.glad.channel.wechat.api;
+
+/**
+ * @author Cartoon
+ *         on 2015/4/9.
+ */
+public interface WeChat {
+}
